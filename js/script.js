081969405bf7def9d7ad02f87edd089e8fdceacf
FULLNAM-O76QP1B\Administrator <hyulives@gmail.com>
2017-09-09 11:11:42 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*
+    소스등을관리하고 유지하기 위한 내용
+    버전관리(v 1.0)
+    협업 (소스 공유)
+    
+    git(bash) -> service(ui) -> github(gitlab)
+    github 상용서비스
+    gitlab 개인서버 설치
+    
+    node, react, router, express, -> 포트폴리오
+    
+    wiki(문서), jira(업무-티켓), slack(메신져), trello(일정관리)
+*/
+
+/*
+    1. github.com 에 회원가입
+    2. sourcetree 다운 후 회원가입
+    3. github에 repository를 만든다.
+*/
